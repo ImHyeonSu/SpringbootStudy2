c437d34ca7f3d61e48b3470eeacc5767e4fec9cd
ImHyeonSu <dlagustn55@naver.com>
2023-04-18 20:33:14 +0900

    2023-04-18 update

--- src/main/java/com/example/demo/Controller/registerConteroller.java
+++ src/main/java/com/example/demo/Controller/registerConteroller.java
@@ -13,17 +13,40 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class RegisterConteroller {
-	
+
 	private final RegisterService registerService;
 
 	@GetMapping("/register")
 	public String registerPage() {
 		return "register/register";
 	}
-	
+
 	@PostMapping("/register")
 	public String register(Members member){
 		return registerService.registerTodo(member);
 	}
 
+	@GetMapping("/emailCheck")
+	@ResponseBody
+	public String memberEmailCheckPost(@RequestParam("memberEmail")String memberemail)throws Exception{
+		int result = registerService.emailCheck(memberemail);
+		
+		if(result == 0) {
+			return "fail";
+		}else {
+			return "success";
+		}
+	}
+	@GetMapping("/nicknameCheck")
+	@ResponseBody
+	public String memberNicknameCheckPost(@RequestParam("memberNickname")String membernickname)throws Exception{
+		int result = registerService.nicknameCheck(membernickname);
+		
+		if(result == 0) {
+			return "fail";
+		}else {
+			return "success";
+		}
+	}
+	
 }
--- src/main/java/com/example/demo/Service/registerService.java
+++ src/main/java/com/example/demo/Service/registerService.java
@@ -30,9 +30,76 @@ public class RegisterService {
 		member.setCommonCode("00003");
 		member.setMemState("0");
 		member.setMemPoint(0);
+		//telephoneHipenX(member);
+		memberRepository.save(member);
 		return "redirect:/register";
-		
 	}
 	
+	public String telephoneHipenX(Members member) {
+		String telephone = member.getMemPhone();
+		telephone = telephone.replaceAll("-","");
+		member.setMemPhone(telephone);
+		return telephone;
+	}
+	
+	
+}
+
+package com.example.demo.Service;
+
+import java.util.Date;
+
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RegisterService {
+	
+	SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+	Date time = new Date();
+	String localTime = format.format(time);
+	
+	private final MemberRepository memberRepository;
+	
+	
+	public String registerTodo(Members member) {
+		member.setMemRegDate(localTime);
+		member.setCommonCode("00003"); //　更新必要
+		member.setMemState("0"); // 更新必要
+		member.setMemPoint(0);
+		telephoneHipenX(member);
+		memberRepository.save(member);
+		return "redirect:/register";
+	}
+	
+	public String telephoneHipenX(Members member) {
+		String telephone = member.getMemPhone();
+		telephone = telephone.replaceAll("-","");
+		member.setMemPhone(telephone);
+		return telephone;
+	}
+	
+	public int emailCheck(String memberemail) {
+		int result = 0;
+		List<Members> memberList = memberRepository.findBymemEmail(memberemail);
+		if (memberList.isEmpty()) {
+			result = 1;
+			return result;
+		}else {
+			result = 0;
+			return result;
+		}
+	}
+	public int nicknameCheck(String membernickname) {
+		int result = 0;
+		List<Members> memberList = memberRepository.findBymemNick(membernickname);
+		if (memberList.isEmpty()) {
+			result = 1;
+			return result;
+		}else {
+			result = 0;
+			return result;
+		}
+	}
 	
 }
